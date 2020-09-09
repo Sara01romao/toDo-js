3eace8dcbdb59da118cdab8b5e52f5a14b63f015
sara01romao <sara-romao@live.com>
2020-09-09 15:05:33 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,17 @@
+// div header
+
+const clear= document.querySelector(".clear"); //para limpa
+
+const dateElement = document.getElementById("date");
+//--------
+
+
+//lista
+const list = document.getElementById("list");
+
+
+
+
+//final add nota
+
+const input = document.getElementById("list");
