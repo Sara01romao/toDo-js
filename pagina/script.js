8f3fb543b6bfefdfbfcc063c07029c56db2cdcbb
sara01romao <sara-romao@live.com>
2020-09-14 17:08:52 -0300

    .

---
+++ pagina/script.js
@@ -0,0 +1,26 @@
+// div header
+
+const clear= document.querySelector(".clear"); //para limpa
+
+const dateElement = document.getElementById("date");
+//--------
+
+
+//lista
+const list = document.getElementById("list");
+
+   const text = <li class= "item">
+         <i class= "co fa fa-circle-thin" job="complete"></i>
+         <p class= "text">Drink Coffee</p>
+         <i class= "de fa fa-trash-o" job="delete"></i>
+   </li>
+
+   list.insertAdjacentHTML(position,text);
+
+
+//final add nota
+
+const input = document.getElementById("input");
+
+
+
